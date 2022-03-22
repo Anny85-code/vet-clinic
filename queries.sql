95bc6c3ff28be22a3f332c7bbf2d4ef39f64ad53
uae <aniekaudo@yahoo.com>
2022-03-22 12:06:05 +0100

    :card_file_box: Update database
    Made changes to the databse

--- queries.sql
+++ queries.sql
@@ -1,5 +1,5 @@
 SELECT * FROM animals WHERE name like '%mon';
-SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
+SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 SELECT * FROM animals WHERE neutered = true AND escape_attempts < 3;
 SELECT date_of_birth, name FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
