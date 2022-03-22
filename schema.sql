f4a65926296999598183b5ecfdd3aa4354308f0b
uae <aniekaudo@yahoo.com>
2022-03-22 11:38:16 +0100

    :card_file_box: Data base
    Adding tables and queries in database

--- schema.sql
+++ schema.sql
@@ -1,9 +1,10 @@
 CREATE TABLE animals (
- id INT GENERATE ALWAYS AS IDENTITY,
+ id INT GENERATED ALWAYS AS IDENTITY,
  name VARCHAR(30),
  date_of_birth DATE,
  escape_attempts INT,
  neutered BOOLEAN,
- weight_kg DECIMAL
+ weight_kg DECIMAL,
+ PRIMARY KEY (id)
 );
 
