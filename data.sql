ae3bb410603cd0196ce59bfb9fc7fd432c1aa036
uae <aniekaudo@yahoo.com>
2022-03-24 17:53:12 +0100

    :card_file_box: Inserted data
    Inserted data into specialization table

--- data.sql
+++ data.sql
@@ -25,3 +25,5 @@ UPDATE animals SET owners_id = 4 WHERE name IN ('Charmander','Squirtle','Blossom
 UPDATE animals SET owners_id = 5 WHERE name IN ('Angemon','Boarmon');
 
 INSERT INTO vets(name, age, date_of_graduation) VALUES('William Tatcher', 45, '2000-04-23'),('Maisy Smith', 26, '2019-01-17'),('Stephanie Mendez', 64, '1981-05-04'),('Jack Harkness', 38, '2008-06-08');
+
+INSERT INTO specializations(vets_id, species_id) VALUES(1, 1),(3, 2),(3, 1),(4,2);
